63172918eda4c5394306b814b2faa7724d55d1fa
VerliererK <r03548014@ntu.edu.tw>
2025-11-27 03:03:05 +0800

    feat: update alias path to src/web and configure Vite server to bind to all interfaces

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,10 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src/web', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+  }
 })
