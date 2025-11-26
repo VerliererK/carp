761747a8ca80c6a6edbfe3e99da60425f254eb79
VerliererK <r03548014@ntu.edu.tw>
2025-11-27 03:02:29 +0800

    feat: add TailwindCSS and Iconify Vue support

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 import { cloudflare } from "@cloudflare/vite-plugin"
 
@@ -9,6 +10,7 @@ import { cloudflare } from "@cloudflare/vite-plugin"
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     cloudflare()
   ],
   resolve: {
