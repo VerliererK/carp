92a8a58a9d6f594a69edb476d19101b136bd8212
VerliererK <r03548014@ntu.edu.tw>
2025-12-27 19:55:51 +0800

    feat(web): add favicon and update logo component

--- src/web/components/Logo.vue
+++ src/web/components/Logo.vue
@@ -1,6 +1,9 @@
 <template>
   <div
-    class="w-10 h-10 shrink-0 bg-linear-to-br from-[hsl(var(--p-blue-500))] to-[hsl(var(--p-blue-700))] rounded-xl flex items-center justify-center shadow-lg text-white font-bold text-2xl border border-white/10">
-    C
+    class="w-10 h-10 shrink-0 bg-linear-to-br from-[hsl(var(--p-blue-500))] to-[hsl(var(--p-blue-700))] rounded-xl flex items-center justify-center">
+    <svg viewBox="0 0 32 32" class="w-8 h-8">
+      <path d="M 6 16 Q 16 4 26 16 Q 16 28 6 16 Z" fill="white" />
+      <circle cx="21" cy="15" r="1.5" fill="hsl(var(--p-blue-500))" />
+    </svg>
   </div>
 </template>
